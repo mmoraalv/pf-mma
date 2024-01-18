e822e2c57ae5fbfe1f224bdc3c9779d00ead7454
mariomora <m.mora.alv@gmail.com>
2024-01-18 13:30:20 -0600

    Se hacen cambios al test

---
+++ test/sessions/Sessions.test.js
@@ -0,0 +1,150 @@
+import 'dotenv/config';
+import chai from 'chai';
+import supertest from 'supertest';
+import mongoose from 'mongoose';
+
+const expect = chai.expect;
+const requester = supertest('http://localhost:3000');
+
+await mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('DB conectada'))
+	.catch(error => console.log(`Error en conexión a MongoDB Atlas:  ${error}`));
+
+describe('Testing Aplicación', () => {
+	describe('Test de registro de usuario', () => {
+		let token = '';
+		let cookie = '';
+		before(async function () {
+			this.timeout(7000);
+			const response = await requester.post('/api/session/login').send({
+				email: process.env.ADMIN_USER,
+				password: process.env.ADMIN_PASSWORD,
+			});
+			cookie = response.headers['set-cookie'][0];
+			token = cookie.split(';')[0].split('=')[1];
+			expect(token).to.be.ok;
+		});
+
+		let uid;
+		it('Test endpoint /api/session/register, se espera que cree un usuario', async function () {
+			this.timeout(5000);
+
+			const newUser = {
+				first_name: 'Pedro',
+				last_name: 'Lopez',
+				email: 'pedro@lopez.com',
+				age: 35,
+				password: '3456',
+			};
+
+			const response = await requester.post('/api/session/register').send(newUser);
+			uid = response.body.user._id;
+			expect(response.body.mensaje).to.equal('Usuario creado');
+		});
+
+		it('Test endpoint /api/users/:uid, se espera que elimine un usuario', async function () {
+			const response = await requester.delete(`/api/users/${uid}`).set('Cookie', cookie);
+			expect(response.body.mensaje).to.equal('Usuario eliminado');
+		});
+
+		describe('Test de rutas de productos', () => {
+			let token = '';
+			let pid = '';
+			let cookie = '';
+			before(async function () {
+				this.timeout(7000);
+				const response = await requester.post('/api/session/login').send({
+					email: process.env.ADMIN_USER,
+					password: process.env.ADMIN_PASSWORD,
+				});
+				cookie = response.headers['set-cookie'][0];
+				token = cookie.split(';')[0].split('=')[1];
+				expect(token).to.be.ok;
+			});
+
+			it('Test endpoint /api/products, debe crear un nuevo producto', async function () {
+				const newProduct = {
+					title: 'Monitor',
+					description: 'Monitor Curvo 29 pulgadas',
+					category: 'Monitores',
+					price: 1500,
+					stock: 10,
+					code: 'MON567',
+				};
+				const response = await requester
+					.post('/api/products')
+					.set('Cookie', cookie)
+					.send(newProduct);
+				pid = response.body.product._id;
+				expect(response.body.mensaje).to.equal('Producto creado');
+			});
+
+			it('Test endpoint /api/products/:id, debe modificar un producto', async function () {
+				const updateProduct = {
+					title: 'Monitor',
+					description: 'Monitor Curvo 29 pulgadas',
+					category: 'Monitores',
+					price: 1500,
+					stock: 10,
+					code: 'MON567',
+				};
+				const response = await requester
+					.put(`/api/products/${pid}`)
+					.set('Cookie', cookie)
+					.send(updateProduct);
+				expect(response.body.mensaje).to.equal('Producto actualizado');
+			});
+
+			it('Test endpoint /api/products/:id, Debe eliminar un producto por su id', async function () {
+				const response = await requester
+					.delete(`/api/products/${pid}`)
+					.set('Cookie', cookie);
+				expect(response.body.mensaje).to.equal('Producto eliminado');
+			});
+		});
+
+		describe('Test de rutas de carts', () => {
+			let token = '';
+			let cookie = '';
+			const cid = process.env.TEST_CID;
+
+			before(async function () {
+				this.timeout(7000);
+
+				const response = await requester.post('/api/session/login').send({
+					email: process.env.TEST_USER,
+					password: process.env.TEST_PASSWORD,
+				});
+				cookie = response.headers['set-cookie'][0];
+				token = cookie.split(';')[0].split('=')[1];
+				expect(token).to.be.ok;
+			});
+
+			it('Test endpoint /api/carts/:cid/product/:pid, debe agregar un nuevo producto al carrito', async function () {
+				const pid = '64f65fa3bde9c7cc7fc4de3f';
+				const response = await requester
+					.post(`/api/carts/${cid}/product/${pid}`)
+					.set('Cookie', cookie);
+				expect(response.body.resultado).to.equal('OK');
+			});
+
+			it('Test endpoint /api/carts/:cid/products/:pid, debe modificar la cantidad de un producto al carrito', async function () {
+				const pid = '64f65fa3bde9c7cc7fc4de3f';
+				const response = await requester
+					.put(`/api/carts/${cid}/product/${pid}`)
+					.set('Cookie', cookie)
+					.send({ quantity: 8 });
+				expect(response.body.message.products[0].quantity).to.equal(9);
+			});
+
+			it('Test endpoint /api/carts/:cid/product/:pid, debe eliminar un producto al carrito', async function () {
+				const pid = '64f65fa3bde9c7cc7fc4de3f';
+				const response = await requester
+					.delete(`/api/carts/${cid}/product/${pid}`)
+					.set('Cookie', cookie);
+				expect(response.body.resultado).to.equal('OK');
+			});
+		});
+	});
+});
